68eab8f726f971dcfb54296240c0232c759b96cb
Jackson Lopes <jacksonlopes@gmail.com>
2019-07-20 07:18:40 -0300

    Change text info

--- makeThumbVid.sh
+++ makeThumbVid.sh
@@ -13,8 +13,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Author: Jackson Lopes <jacksonlopes@gmail.com>
-# Site: https://jslabs.cc
-# Source: https://github.com/jacksonlopes/makeThumbVid
+# URL: https://jslabs.cc
+# src: https://github.com/jacksonlopes/makeThumbVid
 
 # Dependencies | Dependências:
 # ffprobe, ffmpeg, mplayer, ImageMagick {montage/convert}
